cffc728d4f59194d71d21f9ff5a8acc7167b1643
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-09-29 06:02:33 +0300

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,25 +1,63 @@
-        for (int item : data) {
-        int[] data = new int[10];
-        }
-public class RandomDataGenerator {
-}
-    }
-        for (int i = 0; i < 10; i++) {
-    public static void main(String[] args) {
             data[i] = (int) (Math.random() * 100) + 1;
-
-
+    }
         }
-            System.out.println("Random Number: " + item);    data = [random.randint(1, 100) for _ in range(10)]
-import random
-def generate_random_data():
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
 
-if __name__ == "__main__":
+        for (int item : data) {
 
-    return data
+}
+        }}
+        int[] data = new int[10];
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+public class RandomDataGenerator {
+        }
+
+    public static void main(String[] args) {
+
+            System.out.println("Random Number: " + item);
+    }
+        }    public static void main(String[] args) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+public class RandomDataGenerator {
+
+        int[] data = new int[10];
+    }
+}
+
+            System.out.println("Random Number: " + item);
+        }        print(f"Random Number: {item}")
+
     for item in data:
 def main():
     data = generate_random_data()
+    main()
+import random
+def generate_random_data():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+
+if __name__ == "__main__":
+def main():
+import random
+
+
+    for item in data:
+
+    data = generate_random_data()
+def generate_random_data():
         print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
 
     main()
