b918308b27cfa0ed9ef77708e6f272ba53c4c067
Chad Collins <144498558+ChadCollins343@users.noreply.github.com>
2023-10-03 09:10:07 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,38 +1,14 @@
-        int[] data = new int[10];
-        for (int item : data) {
-}
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-        }
-        for (int i = 0; i < 10; i++) {
-    }
-public class RandomDataGenerator {
-            System.out.println("Random Number: " + item);
-
-        }
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
-import random
 def generate_random_data():
-def main():
-    return data
     data = generate_random_data()
-
-    main()
     data = [random.randint(1, 100) for _ in range(10)]
-
-
     for item in data:
-
-    return data
-if __name__ == "__main__":
-        print(f"Random Number: {item}")
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
-def main():
 import random
+    return data
     main()
-    for item in data:
 
-    data = generate_random_data()
+        print(f"Random Number: {item}")
 
+if __name__ == "__main__":
+
+
+def main():
